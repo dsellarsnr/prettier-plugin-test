13b140486f00ddfc278b58a718339983be9f6530
Dillon Sellars <dsellars@newrelic.com>
2022-09-14 11:10:02 -0400

    More commit

--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,18 @@
-console.log("hello");
+export const doStuff =
+	(options: { something: string; two?: string; three?: string; four?: string; five?: string }) =>
+	async (arg: string) => {
+		try {
+			console.log(`Hello ${arg}`);
+			console.log(`Something ${options.something}`);
+			return "ok";
+		} catch (error) {
+			/* TODO: Handle errors
+			handle name taken errors
+			restricted actions
+			users can't join
+			*/
+			return undefined;
+		}
+	};
+
+doStuff({ something: "test" });
